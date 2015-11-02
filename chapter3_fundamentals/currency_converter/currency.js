d85e435739a5764d40654ae25604145985de1112
twillzy <william.tanzy@gmail.com>
2015-11-02 16:08:28 +1100

    exporting construtor

--- chapter3_fundamentals/currency_converter/currency.js
+++ chapter3_fundamentals/currency_converter/currency.js
@@ -1,13 +1,31 @@
-var canadianDollar = 0.91;
+// var canadianDollar = 0.91;
 
-var roundTwoDecimals = function(amount) {
+// var roundTwoDecimals = function(amount) {
+// 	return Math.round(amount * 100) / 100;
+// };
+
+// exports.canadianToUS = function(canadian) {
+// 	return roundTwoDecimals(canadian * canadianDollar);
+// };
+
+// exports.USToCanadian = function(us) {
+// 	return roundTwoDecimals(us / canadianDollar);
+// };
+
+var Currency = function(canadianDollar) {
+	this.canadianDollar = canadianDollar;
+};
+
+Currency.prototype.roundTwoDecimals = function(amount) {
 	return Math.round(amount * 100) / 100;
 };
 
-exports.canadianToUS = function(canadian) {
-	return roundTwoDecimals(canadian * canadianDollar);
+Currency.prototype.canadianToUS = function(canadian) {
+	return this.roundTwoDecimals(canadian * this.canadianDollar);
 };
 
-exports.USToCanadian = function(us) {
-	return roundTwoDecimals(us / canadianDollar);
+Currency.prototype.USToCanadian = function(us) {
+	return this.roundTwoDecimals(us / this.canadianDollar);
 };
+
+module.exports = Currency;
--- chapter3_fundamentals/currency_converter/main.js
+++ chapter3_fundamentals/currency_converter/main.js
@@ -1,7 +1,14 @@
-var currency = require('./currency');
+// var currency = require('./currency');
 
-console.log('50 Canadian dollars equals this amount of US dollars: ');
+// console.log('50 Canadian dollars equals this amount of US dollars: ');
+// console.log(currency.canadianToUS(50));
+
+// console.log('30 US dollars equals this amount of Canadian dolalrs: ');
+// console.log(currency.USToCanadian(30));
+
+var Currency = require('./currency');
+
+var canadianDollar = 0.91;
+
+var currency = new Currency(canadianDollar);
 console.log(currency.canadianToUS(50));
-
-console.log('30 US dollars equals this amount of Canadian dolalrs: ');
-console.log(currency.USToCanadian(30));
