8df0471a4249f2e855d945669cb332dcd6a5812a
twillzy <william.tanzy@gmail.com>
2015-11-04 19:36:03 +1100

    custom pub sub with telnet

---
+++ chapter3_fundamentals/event_emitter/pubsub_enhanced.js
@@ -0,0 +1,27 @@
+var events = require('events').EventEmitter;
+var net = require('net');
+
+var channel = new events();
+channel.clients = {};
+channel.subscriptions = {};
+
+channel.on('join', function(id, client){
+	this.clients[id] = client;
+	this.subscriptions[id] = function(senderId, message) {
+		if (id !== senderId) {
+			this.clients[id].write(message);
+		}
+	};
+	this.on('broadcast', this.subscriptions[id]);
+});
+
+var server = net.createServer(function(client){
+	var id = client.remoteAddress + ":" + client.remotePort;
+	channel.emit('join', id, client);
+	client.on('data', function(data){
+		data = data.toString();
+		channel.emit('broadcast', id, data);
+	});
+});
+
+server.listen(8888);
