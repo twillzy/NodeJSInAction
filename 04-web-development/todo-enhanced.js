1f0cfd7436a890f32e0c84b1f045bc25113d4367
twillzy <william.tanzy@gmail.com>
2015-11-05 14:38:50 +1100

    todo in node

---
+++ 04-web-development/todo-enhanced.js
@@ -0,0 +1,82 @@
+var http = require('http');
+var items = [];
+
+var server = http.createServer(function(req, res) {
+	if (req.url === '/') {
+		switch(req.method) {
+			case 'GET' :
+				show(res);
+				break;
+			case 'POST' :
+				add(req, res);
+				break;
+			case 'DELETE' :
+				del(res);
+				break;
+			default:
+				badRequest(res);
+		}
+	} else {
+		notFound(res);
+	}
+}).listen(3000);
+
+var show = function(res) {
+	var html = '<html><head><title>Todo List</title></head><body>'
+					 + '<h1>Todo List</h1>'
+					 + '<ul>'
+					 + items.map(function(item){
+					 			return ('<li>' + item + '</li>');
+						 }).join('')
+					 + '</ul>'
+					 + '<form method="post" action="/">'
+					 + '<p><input type="text" name="item" /></p>'
+					 + '<p><input type="submit" value="Add Item" /></p>'
+					 + '</form></body></html>';
+
+	res.setHeader('Content-Type', 'text/html');
+	res.setHeader('Content-Length', Buffer.byteLength(html));
+	res.end(html);
+};
+
+var notFound = function(res) {
+	res.statusCode = 404;
+	res.setHeader('Content-Type', 'text/plain');
+	res.end('Not Found');
+};
+
+var badRequest = function(res) {
+	res.statusCode = 400;
+	res.setHeader('Content-Type', 'text/plain');
+	res.end('Bad Request');
+};
+
+var qs = require('querystring');
+
+var add = function(req, res) {
+	var body = '';
+	req.setEncoding('utf8');
+	req.on('data', function(chunk) {
+		body += chunk;
+	});
+	req.on('end', function(){
+		var obj = qs.parse(body);
+		items.push(obj.item);
+		show(res);
+	});
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
